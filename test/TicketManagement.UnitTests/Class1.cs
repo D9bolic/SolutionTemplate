7d72e906296748133be5ad996f4aa4e27991a300
Krystsina Kurchava <krystsina.kurchava@gmail.com>
2020-10-20 12:59:57 +0300

    Added CI yaml

--- test/TicketManagement.UnitTests/Class1.cs
+++ test/TicketManagement.UnitTests/Class1.cs
@@ -1,6 +1,19 @@
-﻿namespace TicketManagement.UnitTests
+﻿using Xunit;
+
+namespace TicketManagement.UnitTests
 {
     public class Class1
     {
+        [Fact]
+        public void SuccsessTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.False(true);
+        }
     }
 }
